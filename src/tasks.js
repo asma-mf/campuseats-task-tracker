7fbb8b352e00adeff58128647c7c68399852c574
fathi-asma <fathuasma456@gmail.com>
2026-09-20 18:00:22 +0530

    feat: add initial campuseEats task list

---
+++ src/tasks.js
@@ -0,0 +1,8 @@
+"// campuseEats task list"
+
+const tasks = [
+ "Design the menu screen",
+ "Build the orders API",
+ "Add user login",
+];
+console.log(`CampusEats has ${tasks.length} open tasks`);
